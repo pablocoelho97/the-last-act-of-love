5014faf472ad3ed2edda293d2a8ce8e1238d9549
pablocoelho97 <porumnovobrasiloficial@gmail.com>
2026-02-01 04:48:48 +0000

    Update content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./client/index.html",
-    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
